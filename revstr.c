d98c97e4b1ed0fc3426e09659e5229bfb145dc0c
CharuSengar11 <106750524+CharuSengar11@users.noreply.github.com>
2022-06-02 20:11:16 +0530

    Add files via upload

---
+++ revstr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100 // Maximum string size
+
+int main()
+{
+    char str[MAX_SIZE];
+
+    
+    printf("Enter any string: ");
+    gets(str);
+
+    printf("Original string = %s\n", str);
+
+    
+    strrev(str);
+
+    printf("Reverse string = %s", str);
+
+    return 0;
+}
